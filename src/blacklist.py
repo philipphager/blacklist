2daa9119804b80d8af957cd1ab3ef0d5c10b80f5
Philipp Hager <philipp.konstantin.hager@gmail.com>
2022-03-26 23:06:10 +0100

    Initial commit

---
+++ src/blacklist.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class Blacklist:
+    def __init__(self, file):
+        self.file = file
+        self.BEGIN = "# Blacklisted URLs - BEGIN\n"
+        self.END = "# Blacklisted URLs - END\n"
+        self.REDIRECT_IP = "127.0.0.1"
+
+    def set_domains(self, domains):
+        with open(self.file, "r") as f:
+            lines = f.readlines()
+
+        begin_index = lines.index(self.BEGIN) if self.BEGIN in lines else len(lines)
+
+        with open(self.file, "w") as f:
+            # Write original host file until src section
+            for line in lines[:begin_index]:
+                f.write(line)
+
+            # Write src section
+            f.write(self.BEGIN)
+
+            for domain in domains:
+                f.write(f"127.0.0.1 {domain}\n")
+                f.write(f"::1 {domain}\n")
+
+            f.write(self.END)
+
+            # Write rest of original host file
+            if self.END in lines:
+                end_index = lines.index(self.END)
+
+                for line in lines[end_index + 1:]:
+                    f.write(line)
+
+    def get_domains(self) -> List[str]:
+        with open(self.file, "r") as f:
+            lines = f.readlines()
+
+            if self.BEGIN in lines and self.END in lines:
+                start = lines.index(self.BEGIN) + 1
+                end = lines.index(self.END)
+                lines = lines[start:end]
+                return list(set([lines.split()[1].strip() for lines in lines]))
+            else:
+                return []
---
+++ src/main.py
@@ -0,0 +1,43 @@
+import typer
+
+from src.blacklist import Blacklist
+
+app = typer.Typer()
+blacklist = Blacklist("/etc/hosts")
+
+
+@app.command()
+def add(domain: str):
+    domains = blacklist.get_domains()
+
+    if domain in domains:
+        typer.echo(f"{domain} is already blacklisted.")
+    else:
+        domains.append(domain)
+        blacklist.set_domains(domains)
+        typer.echo(f"{domain} added to blacklist.")
+
+
+@app.command()
+def remove(domain: str):
+    domains = blacklist.get_domains()
+
+    if domain in domains:
+        domains.remove(domain)
+        blacklist.set_domains(domains)
+        typer.echo(f"{domain} removed from blacklist.")
+    else:
+        typer.echo(f"{domain} is not blacklisted.")
+
+
+@app.command()
+def show():
+    domains = blacklist.get_domains()
+
+    if len(domains) == 0:
+        typer.echo("No domains are blacklisted.")
+    else:
+        typer.echo(f"List of blacklisted domains:")
+
+        for domain in domains:
+            typer.echo(f" - {domain}")
